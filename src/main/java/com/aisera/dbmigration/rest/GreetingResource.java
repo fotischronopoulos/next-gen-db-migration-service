bdee3f08d124db4ced0d372be715fdded5e49180
Fotis Chronopoulos <fotis.chronopoulos@aisera.com>
2025-11-12 10:32:31 +0200

    logging

--- src/main/java/com/aisera/dbmigration/rest/GreetingResource.java
+++ src/main/java/com/aisera/dbmigration/rest/GreetingResource.java
@@ -6,6 +6,8 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import io.quarkus.logging.Log;
+
 
 @Path("/hello")
 public class GreetingResource {
@@ -16,6 +18,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        Log.info("TEST");
         dbMigrationService.migrate();
         return "Hello from Quarkus REST";
     }
