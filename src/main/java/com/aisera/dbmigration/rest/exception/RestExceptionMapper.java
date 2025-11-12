fa95ab2d3f0231d54ef2ffc6e0a113423d60fb2d
Fotis Chronopoulos <fotis.chronopoulos@aisera.com>
2025-11-12 13:56:06 +0200

    rest exception mapper

---
+++ src/main/java/com/aisera/dbmigration/rest/exception/RestExceptionMapper.java
@@ -0,0 +1,16 @@
+package com.aisera.dbmigration.rest.exception;
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class RestExceptionMapper implements ExceptionMapper<Exception> {
+
+    @Override
+    public Response toResponse(Exception e) {
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity(e.getMessage())
+                .build();
+    }
+}
